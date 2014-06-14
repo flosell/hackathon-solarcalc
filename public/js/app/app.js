78e112c7f6c010b0f1d16c70f081fdf8efeff4bc
thomas <thomas.schiela@gmail.com>
2014-06-14 16:30:17 +0200

    added type radio buttons

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -2,14 +2,42 @@ var solarApp = angular.module('solarcap-app', []);
 
 solarApp.controller('SearchCtrl', [
   '$scope',
-  function($scope){
-    $scope.doSearch = function() {
-        search($scope.address)
+  function ($scope) {
+    $scope.doSearch = function () {
+      search($scope.address)
     }
   }
 ]);
 
-solarApp.directive();
+solarApp.directive('radio', function ($timeout) {
+  return {
+    restrict: 'A',
+    require: '?ngModel',
+    link: function ($scope, $element, $attributes, ngModel) {
+      var value = $attributes.radio;
+
+      $timeout(function () {
+        if (ngModel.$modelValue == value) {
+          $element.addClass('active');
+        }
+      });
+
+      $element.on('click', function () {
+        $('.btn-group .btn').each(function () {
+          $(this).removeClass('active');
+        });
+
+        $element.addClass('active');
+
+        if (ngModel) {
+          $scope.$apply(function () {
+            ngModel.$setViewValue(value);
+          });
+        }
+      });
+    }
+  }
+});
 
 
 
