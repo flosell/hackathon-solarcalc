46f99ca726c8086df2219f4a3762e3651961cc2c
Ingelheim <lukas.ingelheim@gmail.com>
2014-06-14 13:10:54 +0200

    added Jasmine runner and basic calculations for KWH and KWP for each state in Germany

--- public/client.js
+++ public/client.js
@@ -3,3 +3,6 @@ var solarCalcApp = angular.module('solarCalcApp', []);
 solarCalcApp.controller('SolarCalcCtrl', function ($scope) {
   $scope.testMessage = "Hello this is a test message";
 });
+
+
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express  = require('express');
 var _ = require("underscore");
 var app      = express();
 var http = require('http').Server(app);
+var calcs = require('calcs');
 
 app.use(express.static(__dirname + '/public'));
 
