374818ded4b3b13063128ecffb027c43ae497d67
Ingelheim <lukas.ingelheim@gmail.com>
2014-06-14 15:42:13 +0200

    Added basic subsidy calculation

--- solarCalculations/sunCalculator.js
+++ solarCalculations/sunCalculator.js
@@ -2,16 +2,39 @@ var sunCalculator = function(){
   var instance = {};
 
   instance.calculateKWP = function(sqm){
-    return sqm ? formatFloat(sqm * 0.117647, 5) : undefined;
+    return sqm ? formatFloat(sqm * 0.15, 5) : undefined;
   };
 
   instance.calculateKWHYearForKWPForState = function(KWP, state){
-    var KWHperYear = undefined;
+    var KWHperKWP = undefined;
     if (KWP && state) {
-      KWHperYear = getStateKWHData(state) ? formatFloat((getStateKWHData(state) * KWP), 3) : undefined;
+      KWHperKWP = getStateKWHData(state) ? formatFloat((getStateKWHData(state) * KWP), 3) : undefined;
     }
-    return KWHperYear;
+    return KWHperKWP;
   };
+
+  instance.calculateSubsidy = function(KWP, state, sum){
+    var SUBSIDIES = {
+      'small'   : 13.01,
+      'medium'  : 12.34,
+      'large'   : 11.10
+    };
+
+    if(KWP <= 10) {
+      var midSumSmall = instance.calculateKWHYearForKWPForState(KWP, state) * SUBSIDIES['small'] + sum;
+      return formatCentToEuro(midSumSmall);
+    } else if (KWP <= 40){
+      var midSumMedium = instance.calculateKWHYearForKWPForState(KWP - 10, state) * SUBSIDIES['medium'] + sum;
+      return instance.calculateSubsidy(10, state, midSumMedium);
+    } else {
+      var midSumMediumLarge = instance.calculateKWHYearForKWPForState(KWP - 40, state) * SUBSIDIES['large'] + sum;
+      return instance.calculateSubsidy(40, state, midSumMediumLarge);
+    }
+  };
+
+  function formatCentToEuro (cent) {
+    return formatFloat((cent / 100), 2);
+  }
 
   function getStateKWHData(state) {
     return PEAK_DATA[state] ? PEAK_DATA[state] : undefined;
--- spec/calculationSpec.js
+++ spec/calculationSpec.js
@@ -13,15 +13,15 @@ describe('sunCalculator', function(){
     });
 
     it('should return 0.11765 KWp for 1 sqm', function () {
-      expect(calculator.calculateKWP(1)).toEqual(0.11765);
+      expect(calculator.calculateKWP(1)).toEqual(0.15);
     });
 
     it('should return 0.23529 KWp for 2 sqm', function () {
-      expect(calculator.calculateKWP(2)).toEqual(0.23529);
+      expect(calculator.calculateKWP(2)).toEqual(0.30);
     });
 
     it('should return 1 KWp for 8.5 sqm', function () {
-      expect(calculator.calculateKWP(8.5)).toEqual(1);
+      expect(calculator.calculateKWP(10)).toEqual(1.50);
     });
   });
 
@@ -50,4 +50,31 @@ describe('sunCalculator', function(){
       expect(calculator.calculateKWHYearForKWPForState(26, 'Bremen')).toEqual(20829.042);
     });
   });
+
+  describe('calculateSubsidy', function(){
+
+    it('should return 135.56 Euro for 1 KWP in Baden-Wuerttemberg', function () {
+      expect(calculator.calculateSubsidy(1, 'Baden-Württemberg', 0)).toBe(135.56);
+    });
+
+    it('should return 271.12 Euro for 2 KWP in Baden-Wuerttemberg', function () {
+      expect(calculator.calculateSubsidy(2, 'Baden-Württemberg', 0)).toBe(271.12);
+    });
+
+    it('should return 1355.61 Euro for 10 KWP in Baden-Wuerttemberg', function () {
+      expect(calculator.calculateSubsidy(10, 'Baden-Württemberg', 0)).toBe(1355.61);
+    });
+
+    it('should return 1484.19 Euro for 11 KWP in Baden-Wuerttemberg', function () {
+      expect(calculator.calculateSubsidy(11, 'Baden-Württemberg', 0)).toBe(1484.19);
+    });
+
+    it('should return 1484.19 Euro for 60 KWP in Baden-Wuerttemberg', function () {
+      expect(calculator.calculateSubsidy(60, 'Baden-Württemberg', 0)).toBe(7526.17);
+    });
+
+    it('should return 8835.43 Euro for 80 KWP in Sachsen', function () {
+      expect(calculator.calculateSubsidy(80, 'Sachsen', 0)).toBe(8870.19);
+    });
+  })
 });
