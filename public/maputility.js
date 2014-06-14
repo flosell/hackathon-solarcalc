5f25c6e324f044c97ebbc20a83a3ceefcd4edafa
flosell <florian.sellmayr@gmail.com>
2014-06-14 12:08:32 +0200

    putting markers on the map

---
+++ public/maputility.js
@@ -0,0 +1,7 @@
+// JavaScript inheritance helper function
+function extend(B, A) {
+    function I() {}
+    I.prototype = A.prototype;
+    B.prototype = new I();
+    B.prototype.constructor = B;
+}
