17a8cdb3f7139839b9f0a7d1c6b19310a771aa32
flosell <florian.sellmayr@gmail.com>
2014-06-14 20:42:56 +0200

    indentation

--- public/js/solarCalculations/sunCalculator.js
+++ public/js/solarCalculations/sunCalculator.js
@@ -1,35 +1,35 @@
 var sunCalculator = function () {
-    var PEAK_DATA = {
-      'Baden-Württemberg' : 1041.973,
-      'Bavaria' : 1025.547,
-      'Thüringen' : 998.547,
-      'Sachsen-Anhalt': 988.167,
-      'Saarland': 998.113,
-      'Hessen' : 954.843,
-      'Saxony' : 939.340,
-      'Rheinland-Pfalz' : 974.740,
-      'Nordrhein-Westfalen' : 917.513,
-      'Niedersachsen' : 925.137,
-      'Brandenburg' : 895.697,
-      'Hamburg' : 856.610,
-      'Schleswig-Holstein' : 880.323,
-      'Bremen' : 801.117,
-      'Mecklenburg-Vorpommern' : 754.123,
-      'Berlin' : 730.910
-    };
+  var PEAK_DATA = {
+    'Baden-Württemberg' : 1041.973,
+    'Bavaria' : 1025.547,
+    'Thüringen' : 998.547,
+    'Sachsen-Anhalt': 988.167,
+    'Saarland': 998.113,
+    'Hessen' : 954.843,
+    'Saxony' : 939.340,
+    'Rheinland-Pfalz' : 974.740,
+    'Nordrhein-Westfalen' : 917.513,
+    'Niedersachsen' : 925.137,
+    'Brandenburg' : 895.697,
+    'Hamburg' : 856.610,
+    'Schleswig-Holstein' : 880.323,
+    'Bremen' : 801.117,
+    'Mecklenburg-Vorpommern' : 754.123,
+    'Berlin' : 730.910
+  };
 
-    var FIXED_DATE = new Date(2014,5,14);
+  var FIXED_DATE = new Date(2014,5,14);
 
-    var BASE_SUBSIDIES = [13.01, 12.34, 11.01];
+  var BASE_SUBSIDIES = [13.01, 12.34, 11.01];
 
-    var KWH_PER_PERSON = 1200;
+  var KWH_PER_PERSON = 1200;
 
-    var ACQUISITION_COST_PER_KWP_HOME = 1500;
+  var ACQUISITION_COST_PER_KWP_HOME = 1500;
 
-    var ACQUISITION_COST_PER_KWP_FIELD = 1600;
+  var ACQUISITION_COST_PER_KWP_FIELD = 1600;
   var instance = {};
 
-//  FIXED DATE OF SUBSIDIES
+  //  FIXED DATE OF SUBSIDIES
   var TODAY = new Date();
   var SUBSIDIES = undefined;
   var PEOPLE = 0;
