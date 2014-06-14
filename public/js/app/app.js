70e33c3ab789731a2cd9422aa6a11829a40b4f96
thomas <thomas.schiela@gmail.com>
2014-06-14 21:12:16 +0200

    added google charts

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,4 +1,8 @@
-var solarApp = angular.module('solarcap-app', []);
+'use strict';
+
+var solarApp = angular.module('solarcap-app', [
+  'googlechart'
+])
 
 solarApp.controller('SearchCtrl', [
   '$scope',
@@ -7,6 +11,92 @@ solarApp.controller('SearchCtrl', [
       type: 'house',
       residents: 1
     };
+
+    $scope.chartObject = {
+      "type": "AreaChart",
+      "displayed": true,
+      "data": {
+        "cols": [
+          {
+            "id": "time",
+            "label": "Time",
+            "type": "string",
+            "p": {}
+          },
+          {
+            "id": "consumption",
+            "label": "Consumption",
+            "type": "number",
+            "p": {}
+          },
+          {
+            "id": "solar-output",
+            "label": "Solar Output",
+            "type": "number",
+            "p": {}
+          }
+        ],
+        "rows": []
+      },
+      "options": {
+        "title": "Graph",
+        "isStacked": "true",
+        "colors": ['#4A89DC','#2dcff1'],
+        "fill": 40,
+        'chartArea': {'width': '85%', 'height': '80%'},
+        'legend': {'position': 'top'},
+        animation: {
+          duration: 1000,
+          easing: 'out'
+        },
+        fontName: 'Roboto',
+        fontSize: 11,
+        "displayExactValues": true,
+        "vAxis": {
+          "title": "kw/h",
+          "gridlines": {
+            "count": 10
+          }
+        },
+        "hAxis": {
+          "title": "Time"
+        }
+      },
+      "formatters": {}
+    }
+
+
+
+
+    $scope.generateChartDate = function(){
+      var consumption = [
+        0.131, 0.113, 0.090, 0.083,
+        0.064, 0.064, 0.071, 0.079,
+        0.094, 0.124, 0.188, 0.203, 0.203
+      ];
+
+
+      // 500 grundlast + n*1000
+
+
+      for(var i = 0; i <= 12; i++){
+        $scope.chartObject.data.rows.push({
+          "c": [
+            {
+              "v": (i*2) + ':00'
+            },
+            {
+              "v": consumption[i]
+            },
+            {
+              "v": 1
+            }
+          ]
+        });
+      }
+    }
+
+    $scope.generateChartDate();
 
     $scope.setRatingValue = function(n){
       $scope.parameter.residents = n;
@@ -26,7 +116,9 @@ solarApp.controller('MapCtrl', [
         $scope.$apply(function(){
             $scope.selectedArea = data
         })
-    })
+    });
+
+
 
     $scope.$watch("selectedArea",function(data) {
         if (data === undefined) return;
