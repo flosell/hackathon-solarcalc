7b0bad329ddb17fa0a4020726dc8969ec58d2494
Ingelheim <lukas.ingelheim@gmail.com>
2014-06-14 21:25:23 +0200

    Refactoring

--- public/js/solarCalculations/sunCalculator.js
+++ public/js/solarCalculations/sunCalculator.js
@@ -1,24 +1,24 @@
 var sunCalculator = function () {
   var PEAK_DATA = {
-    'Baden-Württemberg' : 1041.973,
-    'Bavaria' : 1025.547,
-    'Thüringen' : 998.547,
+    'Baden-Württemberg': 1041.973,
+    'Bavaria': 1025.547,
+    'Thüringen': 998.547,
     'Sachsen-Anhalt': 988.167,
     'Saarland': 998.113,
-    'Hessen' : 954.843,
-    'Saxony' : 939.340,
-    'Rheinland-Pfalz' : 974.740,
-    'Nordrhein-Westfalen' : 917.513,
-    'Niedersachsen' : 925.137,
-    'Brandenburg' : 895.697,
-    'Hamburg' : 856.610,
-    'Schleswig-Holstein' : 880.323,
-    'Bremen' : 801.117,
-    'Mecklenburg-Vorpommern' : 754.123,
-    'Berlin' : 730.910
+    'Hessen': 954.843,
+    'Saxony': 939.340,
+    'Rheinland-Pfalz': 974.740,
+    'Nordrhein-Westfalen': 917.513,
+    'Niedersachsen': 925.137,
+    'Brandenburg': 895.697,
+    'Hamburg': 856.610,
+    'Schleswig-Holstein': 880.323,
+    'Bremen': 801.117,
+    'Mecklenburg-Vorpommern': 754.123,
+    'Berlin': 730.910
   };
 
-  var FIXED_DATE = new Date(2014,5,14);
+  var FIXED_DATE = new Date(2014, 5, 14);
 
   var BASE_SUBSIDIES = [13.01, 12.34, 11.01];
 
@@ -72,7 +72,7 @@ var sunCalculator = function () {
     TODAY = newDate;
   };
 
-  instance.calculateAmortization = function(acquisitionCost, grossProfit) {
+  instance.calculateAmortization = function (acquisitionCost, grossProfit) {
     return formatFloat(acquisitionCost / grossProfit, 0);
   };
 
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-var express  = require('express');
+var express = require('express');
 var _ = require("underscore");
-var app      = express();
+var app = express();
 var calculator = require('./public/js/solarCalculations/sunCalculator')
 var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', function(req, res) {
-	res.render('index.html');
+app.get('/', function (req, res) {
+  res.render('index.html');
 });
 
-app.get('/api',function(req,res){
-    var area = req.query.area,
-        state = req.query.state,
-        residents = req.query.residents,
-        kind = req.query.kind
+app.get('/api', function (req, res) {
+  var area = req.query.area,
+    state = req.query.state,
+    residents = req.query.residents,
+    kind = req.query.kind;
 
-    var result = calculator().calculateSolarCap(area, state, residents, kind);
+  var result = calculator().calculateSolarCap(area, state, residents, kind);
 
-    res.json(result);
-})
+  res.json(result);
+});
 
 http.listen(8081);
 console.log("App listening on port 8081");
