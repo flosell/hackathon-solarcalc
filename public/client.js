ec7c778a8019af4abfc32b35fefbd604d53fe112
flosell <florian.sellmayr@gmail.com>
2014-06-14 17:01:54 +0200

    removed old files (never used)

--- public/client.js
+++
@@ -1,8 +0,0 @@
-var solarCalcApp = angular.module('solarCalcApp', []);
-
-solarCalcApp.controller('SolarCalcCtrl', function ($scope) {
-  $scope.testMessage = "Hello this is a test message";
-});
-
-
-
